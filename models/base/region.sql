49fdb6ccf93b9b17ab4aea273310bcdcfcfd2cf0
Nicholas Johansen <n@nickjohansen.com>
2021-01-16 00:46:41 +0000

    added region to currency mapping

--- models/base/region.sql
+++ models/base/region.sql
@@ -1,20 +1,25 @@
-WITH region AS (
-SELECT 
-Marketplace AS marketplace,
-Region.Region AS region, 
-BookBub_Region AS bookBub_region
-FROM 
-{{ source('raw', 'region') }}) 
-
-SELECT region.marketplace, currencies.currency, region_to_currency_mapping.country_code
-
-FROM region
-
-LEFT JOIN {{ source('raw', 'region_to_currency_mapping') }} AS region_to_currency_mapping
-ON region.region = region_to_currency_mapping.country_code
-
-LEFT JOIN {{ source('raw', 'currencies') }} AS currencies
-ON currencies.currency = region_to_currency_mapping.currency_code
+WITH
+  region AS (
+  SELECT
+    Marketplace AS marketplace,
+    Region.Region AS region,
+    BookBub_Region AS bookBub_region
+  FROM
+    {{ source('raw',
+      'region') }})
+SELECT
+  region.marketplace,
+  region.region,
+  bookBub_region,
+  region_to_currency_mapping.currency_code
+FROM
+  region
+LEFT JOIN
+  {{ ref('region_to_currency_mapping') }} AS region_to_currency_mapping
+ON
+  region.region = region_to_currency_mapping.country_code
+WHERE
+  marketplace != 'Marketplace'
 
 
 
