b872b2deabc30a87a9e291603441ef324aa7d683
Nicholas Johansen <n@nickjohansen.com>
2021-01-26 23:37:51 +0000

    added title_mappings to fb data

--- models/join/fb_ads_asin_mapping.sql
+++ models/join/fb_ads_asin_mapping.sql
@@ -2,8 +2,10 @@ WITH fb_ads_asin_mapping AS (SELECT * EXCEPT (campaign_id),
 CAST(campaign_id AS INT64) 
 AS campaign_id FROM {{ source('raw', 'fb_ads_asin_mapping') }}) 
 
-SELECT fbm_ad.*, fb_ads_asin_mapping.ASIN_region FROM fb_ads_asin_mapping
+SELECT title, author, fbm_ad.*, fb_ads_asin_mapping.ASIN_region FROM fb_ads_asin_mapping
 
 LEFT JOIN {{ ref('fbm_ad') }} AS fbm_ad
 ON fb_ads_asin_mapping.campaign_id = fbm_ad.campaign_id
--- Needs joining on title_mappings 
+
+LEFT JOIN {{ ref('title_mappings') }} AS title_mappings
+ON fb_ads_asin_mapping.ASIN_region = title_mappings.ASIN_region
