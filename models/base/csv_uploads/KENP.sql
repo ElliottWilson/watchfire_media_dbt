4d35b3d33a94f61bb6c7da258b9c5b068cfc0354
Nicholas Johansen <n@nickjohansen.com>
2021-02-16 01:53:41 +0000

    Updated data source to CIFL

--- models/base/csv_uploads/KENP.sql
+++ models/base/csv_uploads/KENP.sql
@@ -1,4 +1,12 @@
-SELECT
- * 
+WITH kenp_table AS 
+(SELECT
+ *,  
+EXTRACT(MONTH FROM date) AS month,
+EXTRACT(YEAR FROM date) AS year,
 FROM 
-    {{ source('raw', 'kenp') }}
+    {{ source('raw', 'kenp') }}) 
+
+SELECT *, 
+(kenp_table.month ||'-'|| kenp_table.year ) AS month_year
+
+FROM kenp_table
--- models/base/csv_uploads/combined_sales.sql
+++ models/base/csv_uploads/combined_sales.sql
@@ -1,5 +1,22 @@
-SELECT
+WITH combined_sales_id AS 
+
+(SELECT *, md5(name||time_of_entry||royalty_date||title||author_name||ASIN_ISBN||marketplace||
+royalty_type||transaction_type||units_sold||units_refunded||net_units_sold||avg_list_price_without_tax||
+avg_offer_price_without_tax||avg_delivery_manufacturing_cost||royalty||currency) AS id 
+FROM {{ source('raw', 'combined_sales') }}),
+
+entry_time AS 
+(SELECT 
+*,
+MAX(time_of_entry) OVER (PARTITION BY id) AS max_time_of_entry,
+CAST(EXTRACT(MONTH FROM royalty_date) as string) AS month,
+CAST(EXTRACT(YEAR FROM royalty_date)  as string) AS year
+FROM combined_sales_id), 
+
+combined_sales AS 
+ (SELECT
 * EXCEPT (units_sold, units_refunded, net_units_sold, avg_list_price_without_tax, avg_offer_price_without_tax, avg_delivery_manufacturing_cost, royalty),
+  CONCAT(month,"-",year) AS month_year,
   IFNULL(CAST(units_sold AS numeric),0) AS units_sold,
   IFNULL(CAST(units_refunded AS numeric),0) AS units_refunded,
   IFNULL(CAST(net_units_sold AS numeric),0) AS net_units_sold,
@@ -10,5 +27,10 @@ SELECT
   END AS avg_delivery_manufacturing_cost,
   IFNULL(CAST(royalty AS numeric),0) AS royalty
 FROM 
-    {{ source('raw', 'combined_sales') }}
-WHERE Royalty_Date IS NOT NULL
+    entry_time
+WHERE time_of_entry = max_time_of_entry
+AND Royalty_Date IS NOT NULL)
+
+SELECT *
+
+FROM combined_sales
